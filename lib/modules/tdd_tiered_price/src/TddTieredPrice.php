4398c9f5149e8d0ab5be8bc1bcaed7cf1aeb5a2d
Abel Santos <abel.santos.corral@gmail.com>
2023-11-25 18:59:12 +0100

    ddd-001: First version of tiered price TDD.

---
+++ lib/modules/tdd_tiered_price/src/TddTieredPrice.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\tdd_tiered_price;
+
+/**
+ * TDD Tiered price calculator.
+ */
+class TddTieredPrice {
+  /**
+   * @return int
+   *   The tiered price
+   */
+  public function getTieredPrice(): int {
+    return (299);
+  }
+}
